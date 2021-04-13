bb54b463617663a8806d2184de7572de53cb2d1a
quabid <quabidlord@gmail.com>
2021-04-13 10:50:42 -0400

    Updated test

--- objectserializer/src/test/java/com/gmail/quabidlord/objectserializer/SerializerTests.java
+++ objectserializer/src/test/java/com/gmail/quabidlord/objectserializer/SerializerTests.java
@@ -10,24 +10,35 @@ import org.junit.Test;
 public class SerializerTests {
     @Test
     public void shouldReturnPassing() {
-        PrintStream printer  = new PrintStream(System.out);
+        PrintStream printer = new PrintStream(System.out);
         MyConstants constants = new MyConstants();
         Serializer serializer = new Serializer();
         SomeObject someObj = new SomeObject(455, 888);
         PathValidator pathValidator = new PathValidator();
         PathRemover pathRemover = new PathRemover();
-        if (pathValidator.pathExists(constants.deleteSerializedPath)) {
-            pathRemover.removePath(constants.deleteSerializedPath);
-            printer.println("File " + constants.deleteSerializedPath + " deleted.");
-        } 
-        assertFalse(pathValidator.pathExists(constants.destinationPath));
-        serializer.serialize(someObj, constants.destinationPath);  
-        assertTrue(pathValidator.pathExists(constants.destinationPath));
-        assertFalse(!pathValidator.pathExists(constants.destinationPath));
+
         if (pathValidator.pathExists(constants.deleteSerializedPath)) {
             pathRemover.removePath(constants.deleteSerializedPath);
             printer.println("File " + constants.deleteSerializedPath + " deleted.");
         }
-        assertFalse(pathValidator.pathExists(constants.destinationPath));        
+
+        assertFalse(pathValidator.pathExists(constants.destinationPath));
+        serializer.serialize(someObj, constants.destinationPath);
+
+        assertTrue(pathValidator.pathExists(constants.destinationPath));
+        assertFalse(!pathValidator.pathExists(constants.destinationPath));
+
+        try {
+            Thread.sleep(3233);
+        } catch (InterruptedException ie) {
+            return;
+        }
+
+        if (pathValidator.pathExists(constants.deleteSerializedPath)) {
+            pathRemover.removePath(constants.deleteSerializedPath);
+            printer.println("File " + constants.deleteSerializedPath + " deleted.");
+        }
+
+        assertFalse(pathValidator.pathExists(constants.destinationPath));
     }
 }
